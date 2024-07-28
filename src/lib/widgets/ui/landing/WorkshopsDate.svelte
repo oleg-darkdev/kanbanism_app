c137dc140b1d2d999df6b94efa024529e98f2d55
DarkDev <darkdev_brand@riseup.net>
2024-07-28 19:26:45 +0300

    small refactoring layout img & text title

--- src/lib/widgets/ui/landing/WorkshopsDate.svelte
+++ src/lib/widgets/ui/landing/WorkshopsDate.svelte
@@ -1,13 +1,13 @@
 <section class="section-4-wrapper">
   <div class="div-block-12">
     <div class="full-moon-image-container">
-      <img src="images/fullmoon.png" loading="lazy"  alt="" class="image-23">
+      <img src="images/workshop.svg" loading="lazy"  alt="" class="image-23">
       <div class="large-stat-text centered white absolute">7</div>
     </div>
     <div>
-      <a  href="#" class="share-button-container light w-inline-block">
+      <a  href="/" class="share-button-container light w-inline-block">
         <div class="div-block-20">
-          <div class="cta-text">Share</div>
+          <div class="cta-text">Rejestracja</div>
           <div>
             <img src="images/share-icn.svg" loading="lazy" alt="">
           </div>
@@ -18,8 +18,8 @@
   <div class="calendar">
     <img src="images/month-Cal-Container.png" loading="lazy" sizes="(max-width: 479px) 83vw, 25vw"  alt="" class="image-4">
     <div  class="div-block-22">
-      <div class="text-block-2">Następny warsztat</div>
-      <div class="text-block-3">Podczas Agile.nights </div>
+      <div class="text-block-2 text-2xl">Następny warsztat</div>
+      <div class="leading-none text-block-3 ">Podczas Agile.nights </div>
       <div class="text-subtext">Spokojnie, nie możemy podać dokładnych dat dla Agile.nights.</div>
     </div>
   </div>
@@ -33,3 +33,56 @@
     </div>
   </div>
 </div>
+
+
+
+<style>
+  .text-block-2 {
+  text-align: center;
+  padding-top: 5vw;
+  font-family: Recifedisplay, sans-serif;
+  position: relative;
+}
+.text-block-3 {
+  z-index: 2;
+  color: #fff;
+  text-align: center;
+  padding-top: 4vw;
+  padding-bottom: 2vw;
+  font-family: Fellix, sans-serif;
+  font-size: 4em;
+  font-weight: 700;
+  position: relative;
+}
+
+@media screen and (max-width: 991px) {
+  .text-block-2 {
+    padding-top: 4vw;
+  }
+  .text-block-3 {
+    padding-top: 4vw;
+    padding-bottom: 0;
+    font-size: 2em;
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .text-block-2 {
+    padding-top: 4vw;
+  }
+  .text-block-3 {
+    line-height: 0.4em;
+  }
+}
+
+
+@media screen and (max-width: 479px) {
+  .text-block-2 {
+    /* padding-bottom: 25%; */
+    font-size: 1.2em;
+  }
+  .text-block-3 {
+    font-size: 3.4em;
+  }
+}
+</style>
