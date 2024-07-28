7b60bb735ff30b9ca01851acaed94fbfa1a47cf4
DarkDev <darkdev_brand@riseup.net>
2024-07-28 19:25:56 +0300

    refactoring classes, upd text

--- src/lib/widgets/ui/landing/AgileFans.svelte
+++ src/lib/widgets/ui/landing/AgileFans.svelte
@@ -1,5 +1,16 @@
- <div class="section-10-membersince">
+ <div class="section-agile-fans">
     <img src="images/schedule-Icn.png" loading="lazy" alt="" class="image-8">
-    <div class="medium-text">Lorem Ipsum <br> <a href="/">#Agile.fans!</a> </div>
-    <div class="label-top">Lorem Ipsum  <br> is simply dummy  </div>
+    <div class="medium-text">Graj i ucz się, <br> <a href="https://agile-fans.vercel.app/" target="_blank">#Agile.fans!</a> </div>
+    <div class="label-top">najlepsze praktyki <br> społeczności agile </div>
 </div>
+
+
+<style>
+  .section-agile-fans{
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10vh;
+    display: flex;
+  }
+</style>
